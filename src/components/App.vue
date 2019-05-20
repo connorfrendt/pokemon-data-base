ee1da0612a6a3272de01200bf4055f14923b5b10
connorfrendt <cjacksonfx@yahoo.com>
2019-05-20 15:55:05 -0700

    home and login pages hooked up

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+body {
+  background: rgba(255, 0, 0, 0.4);
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router';
+import Login from './components/Login'
+import Home from './components/Home'
+
+export default new VueRouter({
+    routes: [
+        { path: '/', component: Home },
+        { path: '/login', component: Login },
+        { path: '*', redirect: '/' }
+    ]
+});
